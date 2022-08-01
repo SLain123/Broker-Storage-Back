e00ed71254490d9631080592def1fab9e27f8cc6
Artem <sl163@ya.ru>
2022-08-01 13:09:49 +0300

    Cleaned dist files from git;

--- dist/utils/returnValidationResult.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.returnValidationResult = void 0;
-exports.returnValidationResult = (res, err) => res.status(400).json({
-    errors: err.array(),
-    message: 'Data uncorrect!',
-});
-//# sourceMappingURL=returnValidationResult.js.map
