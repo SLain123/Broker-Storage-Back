8a0d85c29d5e80f77ef0a2f7d1f8e5ef666ea83e
Artem <sl163@ya.ru>
2022-08-10 12:42:43 +0300

    Fixes routes for getting all stock by user or concrete stock according to new Stock model with history;

--- routes/stock.routes.ts
+++ routes/stock.routes.ts
@@ -14,170 +14,174 @@ import { getDeltaCount, IStockData } from './../utils/getDeltaCount';
 
 const router = Router();
 
-// // /api/stock
-// router.post(
-//     '/',
-//     [
-//         check('id', 'ID was not recived or incorrect').custom((id) =>
-//             Types.ObjectId.isValid(id),
-//         ),
-//     ],
-//     checkAuth,
-//     async (req: Request, res: Response) => {
-//         try {
-//             const errors = validationResult(req);
-//             if (!errors.isEmpty()) {
-//                 return returnValidationResult(res, errors);
-//             }
+// /api/stock
+router.post(
+    '/',
+    [
+        check('id', 'ID was not recived or incorrect').custom((id) =>
+            Types.ObjectId.isValid(id),
+        ),
+    ],
+    checkAuth,
+    async (req: Request, res: Response) => {
+        try {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return returnValidationResult(res, errors);
+            }
 
-//             const result = await User.findById(req.user.userId).populate([
-//                 {
-//                     path: 'stocks',
-//                     populate: {
-//                         path: 'broker',
-//                         populate: { path: 'currency' },
-//                     },
-//                 },
-//                 { path: 'stocks', populate: 'currency' },
-//             ]);
+            const result = await User.findById(req.user.userId).populate([
+                {
+                    path: 'stocks',
+                    populate: {
+                        path: 'broker',
+                        populate: { path: 'currency' },
+                    },
+                },
+                { path: 'stocks', populate: 'currency' },
+                {
+                    path: 'stocks',
+                    populate: { path: 'history', model: 'Stock_History' },
+                },
+            ]);
 
-//             if (!result) {
-//                 return return400(res, 'User not found');
-//             }
-//             const { stocks } = result;
-//             const stock = stocks.filter(
-//                 ({ _id }) =>
-//                     String(new Types.ObjectId(req.body.id)) ===
-//                     String(new Types.ObjectId(_id)),
-//             );
+            if (!result) {
+                return return400(res, 'User not found');
+            }
+            const { stocks } = result;
+            const stock = stocks.filter(
+                ({ _id }) =>
+                    String(new Types.ObjectId(req.body.id)) ===
+                    String(new Types.ObjectId(_id)),
+            );
 
-//             if (stock.length < 1) {
-//                 return return400(res, 'Stock not found');
-//             }
+            if (stock.length < 1) {
+                return return400(res, 'Stock not found');
+            }
 
-//             return res.json({
-//                 message: 'Stoсk was found',
-//                 stock,
-//             });
-//         } catch (e) {
-//             res.status(500).json({ message: 'Something was wrong...' });
-//         }
-//     },
-// );
+            return res.json({
+                message: 'Stoсk was found',
+                stock,
+            });
+        } catch (e) {
+            res.status(500).json({ message: 'Something was wrong...' });
+        }
+    },
+);
 
-// // /api/stock/all
-// router.post(
-//     '/all',
-//     [
-//         check('filters', 'Incorrect filter format or no existing filter name')
-//             .optional()
-//             .isObject()
-//             .custom(
-//                 (obj: Object) =>
-//                     obj.hasOwnProperty('brokerId') ||
-//                     obj.hasOwnProperty('currencyId') ||
-//                     obj.hasOwnProperty('year') ||
-//                     obj.hasOwnProperty('isSold') ||
-//                     obj.hasOwnProperty('type'),
-//             ),
-//     ],
-//     checkAuth,
-//     async (req: Request, res: Response) => {
-//         try {
-//             const errors = validationResult(req);
-//             if (!errors.isEmpty()) {
-//                 return returnValidationResult(res, errors);
-//             }
+// /api/stock/all
+router.post(
+    '/all',
+    [
+        check('filters', 'Incorrect filter format or no existing filter name')
+            .optional()
+            .isObject()
+            .custom(
+                (obj: Object) =>
+                    obj.hasOwnProperty('brokerId') ||
+                    obj.hasOwnProperty('currencyId') ||
+                    obj.hasOwnProperty('year') ||
+                    obj.hasOwnProperty('status') ||
+                    obj.hasOwnProperty('type'),
+            ),
+    ],
+    checkAuth,
+    async (req: Request, res: Response) => {
+        try {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return returnValidationResult(res, errors);
+            }
 
-//             const result = await User.findById(req.user.userId).populate([
-//                 {
-//                     path: 'stocks',
-//                     populate: {
-//                         path: 'broker',
-//                         populate: { path: 'currency' },
-//                     },
-//                 },
-//                 { path: 'stocks', populate: 'currency' },
-//             ]);
+            const result = await User.findById(req.user.userId).populate([
+                {
+                    path: 'stocks',
+                    populate: {
+                        path: 'broker',
+                        populate: { path: 'currency' },
+                    },
+                },
+                { path: 'stocks', populate: 'currency' },
+            ]);
 
-//             if (!result) {
-//                 return return400(res, 'User not found');
-//             }
-//             const { filters } = req.body;
-//             let { stocks } = result;
+            if (!result) {
+                return return400(res, 'User not found');
+            }
+            const { filters } = req.body;
+            let { stocks } = result;
 
-//             if (filters) {
-//                 if (filters.hasOwnProperty('brokerId')) {
-//                     const { brokerId } = filters;
-//                     if (!Types.ObjectId.isValid(brokerId)) {
-//                         return return400(res, 'Wrong broker id format');
-//                     } else {
-//                         stocks = stocks.filter(
-//                             ({ broker }) =>
-//                                 String(new Types.ObjectId(broker._id)) ===
-//                                 String(new Types.ObjectId(brokerId)),
-//                         );
-//                     }
-//                 }
+            if (filters) {
+                if (filters.hasOwnProperty('brokerId')) {
+                    const { brokerId } = filters;
+                    if (!Types.ObjectId.isValid(brokerId)) {
+                        return return400(res, 'Wrong broker id format');
+                    } else {
+                        stocks = stocks.filter(
+                            ({ broker }) =>
+                                String(new Types.ObjectId(broker._id)) ===
+                                String(new Types.ObjectId(brokerId)),
+                        );
+                    }
+                }
 
-//                 if (filters.hasOwnProperty('currencyId')) {
-//                     const { currencyId } = filters;
-//                     if (!Types.ObjectId.isValid(currencyId)) {
-//                         return return400(res, 'Wrong currency id format');
-//                     } else {
-//                         stocks = stocks.filter(
-//                             ({ currency }) =>
-//                                 String(new Types.ObjectId(currency._id)) ===
-//                                 String(new Types.ObjectId(currencyId)),
-//                         );
-//                     }
-//                 }
+                if (filters.hasOwnProperty('currencyId')) {
+                    const { currencyId } = filters;
+                    if (!Types.ObjectId.isValid(currencyId)) {
+                        return return400(res, 'Wrong currency id format');
+                    } else {
+                        stocks = stocks.filter(
+                            ({ currency }) =>
+                                String(new Types.ObjectId(currency._id)) ===
+                                String(new Types.ObjectId(currencyId)),
+                        );
+                    }
+                }
 
-//                 if (filters.hasOwnProperty('year')) {
-//                     const { year } = filters;
-//                     if (isNaN(+year) || String(year).length !== 4) {
-//                         return return400(res, 'Wrong year format');
-//                     } else {
-//                         stocks = stocks.filter(({ buyDate, sellDate }) => {
-//                             const buyYear = buyDate.getFullYear();
-//                             const sellYear = sellDate && sellDate.getFullYear();
-//                             return buyYear === +year || sellYear === +year;
-//                         });
-//                     }
-//                 }
+                if (filters.hasOwnProperty('year')) {
+                    const { year } = filters;
+                    if (isNaN(+year) || String(year).length !== 4) {
+                        return return400(res, 'Wrong year format');
+                    } else {
+                        stocks = stocks.filter(({ lastEditedDate }) => {
+                            return lastEditedDate.getFullYear() === +year;
+                        });
+                    }
+                }
 
-//                 if (filters.hasOwnProperty('isSold')) {
-//                     const { isSold } = filters;
-//                     stocks = stocks.filter(
-//                         ({ sellDate }) => Boolean(sellDate) === Boolean(isSold),
-//                     );
-//                 }
+                if (filters.hasOwnProperty('status')) {
+                    const { status: stat } = filters;
+                    if (stat !== 'active' && stat !== 'closed') {
+                        return return400(res, 'Unexisting status');
+                    } else {
+                        stocks = stocks.filter(({ status }) => stat === status);
+                    }
+                }
 
-//                 if (filters.hasOwnProperty('type')) {
-//                     const { type: filterType } = filters;
-//                     if (
-//                         filterType !== 'stock' &&
-//                         filterType !== 'bond' &&
-//                         filterType !== 'futures'
-//                     ) {
-//                         return return400(res, 'Unexisting type');
-//                     } else {
-//                         stocks = stocks.filter(
-//                             ({ type }) => type === filterType,
-//                         );
-//                     }
-//                 }
-//             }
+                if (filters.hasOwnProperty('type')) {
+                    const { type: filterType } = filters;
+                    if (
+                        filterType !== 'stock' &&
+                        filterType !== 'bond' &&
+                        filterType !== 'futures'
+                    ) {
+                        return return400(res, 'Unexisting type');
+                    } else {
+                        stocks = stocks.filter(
+                            ({ type }) => type === filterType,
+                        );
+                    }
+                }
+            }
 
-//             return res.json({
-//                 message: 'Stocks were found',
-//                 stocks,
-//             });
-//         } catch (e) {
-//             res.status(500).json({ message: 'Something was wrong...' });
-//         }
-//     },
-// );
+            return res.json({
+                message: 'Stocks were found',
+                stocks,
+            });
+        } catch (e) {
+            res.status(500).json({ message: 'Something was wrong...' });
+        }
+    },
+);
 
 // /api/stock/create
 router.post(
